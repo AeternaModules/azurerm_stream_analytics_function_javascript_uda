93921a276614e9a50e9c68eda05eab623ee14ff5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "stream_analytics_function_javascript_udas" {
-  description = "All stream_analytics_function_javascript_uda resources"
-  value       = azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas
-}
 output "stream_analytics_function_javascript_udas_input" {
-  description = "List of input values across all stream_analytics_function_javascript_udas"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : v.input]
+  description = "Map of input values across all stream_analytics_function_javascript_udas, keyed the same as var.stream_analytics_function_javascript_udas"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : k => v.input }
 }
 output "stream_analytics_function_javascript_udas_name" {
-  description = "List of name values across all stream_analytics_function_javascript_udas"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : v.name]
+  description = "Map of name values across all stream_analytics_function_javascript_udas, keyed the same as var.stream_analytics_function_javascript_udas"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : k => v.name }
 }
 output "stream_analytics_function_javascript_udas_output" {
-  description = "List of output values across all stream_analytics_function_javascript_udas"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : v.output]
+  description = "Map of output values across all stream_analytics_function_javascript_udas, keyed the same as var.stream_analytics_function_javascript_udas"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : k => v.output }
 }
 output "stream_analytics_function_javascript_udas_script" {
-  description = "List of script values across all stream_analytics_function_javascript_udas"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : v.script]
+  description = "Map of script values across all stream_analytics_function_javascript_udas, keyed the same as var.stream_analytics_function_javascript_udas"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : k => v.script }
 }
 output "stream_analytics_function_javascript_udas_stream_analytics_job_id" {
-  description = "List of stream_analytics_job_id values across all stream_analytics_function_javascript_udas"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : v.stream_analytics_job_id]
+  description = "Map of stream_analytics_job_id values across all stream_analytics_function_javascript_udas, keyed the same as var.stream_analytics_function_javascript_udas"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_uda.stream_analytics_function_javascript_udas : k => v.stream_analytics_job_id }
 }
 
